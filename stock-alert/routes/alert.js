0228729b1382e2ceddb62f25a3c8f63d2c316bf9
unknown <tonycruz417@gmail.com>
2020-05-15 09:07:54 -0500

    working on api calls

--- stock-alert/routes/alert.js
+++ stock-alert/routes/alert.js
@@ -1,9 +1,17 @@
+const Axios = require('axios');
 var express = require('express');
 var router = express.Router();
+//API KEY 7PAMNZKPHD66LW9D
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+    const symbol = "JPM"
+    const result = await Axios.get(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&${symbol}=JPM&interval=5min&apikey=7PAMNZKPHD66LW9D`
+    );
+    console.log(result.data["Time Series(Daily"])
+    res.json({
+        date: new Date().toISOString(),
+    })
 });
 
 module.exports = router;
